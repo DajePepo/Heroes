245f4d853e1eb5c23ede0a3a0d5fda7bbe1fff85
Pietro Santececca <Pietro@pedros-mbp.localdomain>
2018-08-25 15:33:32 +0200

    first commit

---
+++ Heroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heroes
+//
+//  Created by Pietro Santececca on 25.08.18.
+//  Copyright © 2018 Tecnojam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
